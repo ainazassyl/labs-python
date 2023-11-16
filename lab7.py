68456fc42dd419e021d9b75311223203c38f249e
ainazassyl <ainazokzko@gmail.com>
2023-11-16 22:42:59 +0600

    commit

--- lab7.py
+++ lab7.py
@@ -10,25 +10,67 @@ import random
 def read_random_line():
     with open("file.txt", "r") as file:
         lines = file.readlines()
-        return random.choice(lines)
+        random_line = random.choice(lines)
+        print(random_line.strip())
     
 # Task3
 def count_uppercase():
     with open("file.txt", "r") as file:
-        data = file.read()
-        count = 0
-        for letter in data:
-            if letter.isupper():
-                count += 1
-        return count
+        content = file.read()
+        uppercase_count = sum(1 for char in content if char.isupper())
+        print("Uppercase character count:", uppercase_count)
     
 # Task4
 def count_lines():
     with open("file.txt", "r") as file:
-        count = 0
-        for line in file:
-            if not line.startswith("D"):
-                count += 1
-        return count
+        count = sum(1 for line in file if not line.lstrip().startswith('D'))
+        print(count)
     
 # Task5
+def count_words_ending_with_F(file_name):
+    with open(file_name, 'r') as file:
+        words = file.read().split()
+        count = sum(1 for word in words if word.endswith(('F', 'f')))
+        print(count)
+
+# Task6
+def count_all_and_none_words(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+        all_count = content.split().count("all")
+        none_count = content.split().count("none")
+        print(f'"all" count: {all_count}, "none" count: {none_count}')
+
+# Task7
+def count_word_frequency(file_name):
+    with open(file_name, 'r') as file:
+        words = file.read().split()
+        word_frequency = {}
+        for word in words:
+            if word in word_frequency:
+                word_frequency[word] += 1
+            else:
+                word_frequency[word] = 1
+        print(word_frequency)
+
+# Task8
+def find_longest_word(file_name):
+    with open(file_name, 'r') as file:
+        words = file.read().split()
+        longest_word = max(words, key=len)
+        print(longest_word)
+
+# Task9
+def correct_characters(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+        corrected_content = content.replace('B', 'J')
+        print(corrected_content)
+
+# Task10
+def count_A_and_B_occurrences(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+        count_A = content.lower().count('a')
+        count_B = content.lower().count('b')
+        print(f'a={count_A}, b={count_B}')
--- test.py
+++
@@ -1,8 +0,0 @@
-def get_words_starting_with_vowel(words):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    vowel_words = [word for word in words if word[0].lower() in vowels]
-    return vowel_words
-
-input_words = ["apple", "banana", "orange", "bear", "cat"]
-result = get_words_starting_with_vowel(input_words)
-print(result)
